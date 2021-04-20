da16f79c1b763abdf8b6829f7bb11455b7cf6210
Rick Lan <ricklan_blue@hotmail.com>
2021-04-20 12:50:48 -0400

    Finished project section

--- src/Components/Work_History/work.jsx
+++ src/Components/Work_History/work.jsx
@@ -1,14 +1,7 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-
-}));
 
 export default function Work() {
-    const classes = useStyles();
-
     return (
-        <div className={classes.header}>
+        <div>
         </div>
     )
 }
