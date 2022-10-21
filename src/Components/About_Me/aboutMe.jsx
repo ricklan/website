3a16303b9009fe7815d1c1275b96eda6640a6572
Rick Lan <ricklan_blue@hotmail.com>
2022-10-21 17:07:13 -0400

    Adding build files

--- src/Components/About_Me/aboutMe.jsx
+++ src/Components/About_Me/aboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
   return (
     <div id="aboutMe">
       <h1 className="title">About Me</h1>
-      Hi, I'm Rick Lan, a 4th year cs student at UofT. My passion in cs lies
+      Hi, I'm Rick Lan, a recent graduate from UofT. I'm currently working at Amazon as a SDE 1. My passion in cs lies
       mainly in web dev. Outside of school, my hobbies include playing card and
       board games as well as hiking.
     </div>
--- src/Components/Work_History/work.jsx
+++ src/Components/Work_History/work.jsx
@@ -2,155 +2,124 @@ import "./work.css";
 import "../../Global/global.css";
 import "../../Global/title.css";
 
+const info = {
+  Amazon: {
+    company: "Amazon Retail",
+    position: "SDE 1",
+    languages: ["Springboot", "React", "TypeScript", "Git", "AWS"],
+    tenure: "June - Nov 2022",
+    tasks: {
+      task1:
+        "Decreased traffic latency by 20% by migrating the usage of a new Amazon translation tool",
+      task2:
+        "Reduced customer complaints by 50% by investigating and fixing the root causes of the most common user defects",
+      task3:
+        "Coordinated and led a major project across half a dozen teams to ensure 25% more vendors are paid on time",
+    },
+  },
+  CSCB20: {
+    company: "UTSC",
+    position: "CSCB20 Teaching Assistant",
+    languages: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Flask",
+      "SQL",
+      "Git",
+      "Web Development",
+    ],
+    tenure: "January - April 2022",
+    tasks: {
+      task1:
+        "Taught HTML, CSS, JavaScript, Flask, SQL, Git and Web Development to 50% of the class, helping them understand concepts and lecture materials better",
+      task2:
+        "Took the initiative to create and publish supplemental lecture videos on web development and SQL, helping 75% more students understand the concepts better",
+      task3:
+        "Answered questions on the course discussion board quickly, decreasing wait time by 20%",
+    },
+  },
+  CSCC43: {
+    company: "UTSC",
+    position: "CSCC43 Teaching Assistant",
+    languages: [
+      "SQL",
+      "PostgreSQL",
+      "ER Diagrams",
+      "Database Design Principles",
+    ],
+    tenure: "Sept 2021 - January 2022",
+    tasks: {
+      task1:
+        "Taught SQL, ER diagrams and database design practices to 50% of the class, helping them understand concepts and lecture materials better",
+      task2:
+        "Took the initiative to create and publish supplemental lecture notes, helping 60% more students understand the concepts better",
+      task3:
+        "Answered questions on the course discussion board quickly, decreasing wait time by 20%",
+    },
+  },
+  PSPC: {
+    company: "PSPC",
+    position: "Fullstack Developer",
+    languages: ["Vue.JS", "Laravel", "Microsoft SQL Server", "Git/Bitbucket"],
+    tenure: "May 2021 - January 2022",
+    tasks: {
+      task1:
+        "Designed a financial reporting template using Vue, Laravel, and Microsoft SQL Server achieving a 75% reduction in submission time for clients",
+      task2:
+        "Overhauled several websites by refactoring their codebase to make it more readable, modular and efficient, improving response times by 30%",
+      task3:
+        "Spearheaded the initiative to update my team's internal documentation, create a Git guide and teach web development and SQL to other interns, reducing their training time by 25%",
+    },
+  },
+  TD: {
+    company: "TD Canada Trust",
+    position: "L2 IT Analyst",
+    languages: ["Python", "Unix"],
+    tenure: "May 2019 - January 2020",
+    tasks: {
+      task1:
+        "Worked in a fast paced environment to provide support for web applications run on Unix servers, improving uptime to 95%",
+      task2:
+        "Coordinated deployment of new features with multiple teams to minimize effort and ensure completion within 8 hours",
+      task3:
+        "Reduced onboarding time for new team members by 50% by spearheading the initiative to author a holistic training and documentation package",
+      task4:
+        "Increased my team's productivity by over 75% by implementing a Python library to automate labour intensive tasks",
+    },
+  },
+};
+
+let counter = 1;
+function getDirection() {
+  let direction = counter % 2 === 0 ? "left" : "right";
+  counter += 1;
+  return direction;
+}
+
 export default function Work() {
   return (
     <div>
       <h1 className="title"> Work Experience </h1>
-      <p> Click on each box for more info </p>
 
       <div className="timeline">
-        {/* CSCB20 TA */}
-        <div className="container left">
-          <div className="event">
-            <p className="place">
-              <b> UTSC Teaching Assistant for CSCB20 </b> <br></br> (January -
-              April 2022)
-            </p>
-            <ul className="description">
-              <li className="task">
-                Taught SQL, HTML, CSS, JavaScript and Flask 50% of the class,
-                helping them understand concepts and lecture materials better
-              </li>
+        {Object.keys(info).map((key, i) => {
+          return (
+            <div className={"container " + getDirection()} key={i}>
+              <div className="event">
+                <p className="place">
+                  <b>{info[key].company}</b> - {info[key].position} <br></br> (
+                  {info[key].tenure})
+                </p>
 
-              <li className="task">
-                Took the initiative to create and publish supplemental lecture
-                videos on web dev and SQL, helping 30% more students understand
-                the concepts better
-              </li>
-
-              <li className="task">
-                Answered questions on the course discussion board quickly,
-                decreasing wait time by 20%
-              </li>
-            </ul>
-          </div>
-        </div>
-        {/* CSCC43 TA */}
-        <div className="container right">
-          <div className="event">
-            <p className="place">
-              <b> UTSC Teaching Assistant for CSCC43 </b> <br></br> (Sept 2021 -
-              January 2022)
-            </p>
-            <ul className="description">
-              <li className="task">
-                Taught SQL, ER diagrams and database design practices to 50% of
-                the class, helping them understand concepts and lecture
-                materials better
-              </li>
-
-              <li className="task">
-                Took the initiative to create and publish supplemental lecture
-                notes, helping 15% more students understand the concepts better
-              </li>
-
-              <li className="task">
-                Answered questions on the course discussion board quickly,
-                decreasing wait time by 20%
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        {/* PSPC */}
-        <div className="container left">
-          <div className="event">
-            <p className="place">
-              <b> Fullstack Developer at PSPC </b> <br></br> (May 2021 - January
-              2022)
-            </p>
-            <ul className="description">
-              <li className="task">
-                Designed a financial reporting template using Vue, Laravel, and
-                Microsoft SQL Server achieving a 75% reduction in submission
-                time for clients
-              </li>
-
-              <li className="task">
-                Overhauled several websites by refactoring their codebase to
-                make it more readable, modular and efficient, improving response
-                times by 30%
-              </li>
-
-              <li className="task">
-                Developed Shell scripts to automate time consuming work,
-                increasing my team’s productivity by 20%
-              </li>
-
-              <li className="task">
-                Spearheaded the initiative to update my team’s internal
-                documentation, create a Git guide and teach web development and
-                SQL to other interns, reducing their training time by 25%
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        {/* TD Work */}
-        <div className="container right">
-          <div className="event">
-            <p className="place">
-              <b> L2 IT Analyst at TD&nbsp;Canada&nbsp;Trust </b> <br></br> (May
-              2019 - January 2020)
-            </p>
-            <ul className="description">
-              <li className="task">
-                Worked in a fast paced environment to provide support for web
-                applications run on Linux servers
-              </li>
-
-              <li className="task">
-                Resolved complex software issues and led deployment changes by
-                communicating effectively with other teams
-              </li>
-
-              <li className="task">
-                Led the initiative to create a Unix training guide and train new
-                team members, allowing them to start working earlier by 2 months
-              </li>
-
-              <li className="task">
-                Implemented a Python library to automate labour&nbsp;intensive
-                tasks, increasing my team’s productivity by over 25%
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        {/* Website */}
-        <div className="container left">
-          <div className="event">
-            <p className="place">
-              <b> UofT Notes Website </b> <br></br> (January 2017 - Present)
-            </p>
-            <ul className="description">
-              <li className="task">
-                Deployed a website using Github Pages to publish and distribute
-                my notes for various classes at UofT
-              </li>
-
-              <li className="task">
-                Increased the marks of students who used my notes to study by
-                10%
-              </li>
-
-              <li className="task">
-                <b>Link: </b>
-                <a href="https://rlqyl.github.io/">UofT Notes Website</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+                {/* Tasks */}
+                {Object.keys(info[key].tasks).map((taskKey, i) => {
+                  return <p key={i}> {info[key].tasks[taskKey]} </p>;
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
